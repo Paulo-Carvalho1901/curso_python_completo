9af01f80b6be947dffb6c0b68738e894e73baea8
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-06-15 11:43:36 -0300

    introdução as variaveis e conceitos bascos

---
+++ 01_logica_programacao_basica/07_inicio_variavel.py
@@ -0,0 +1,18 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar
+# números e underline _.
+# O sinal de = é o operador de atribuição. Ele é usado para
+# atribuir um valor a um nome (variável).
+# Uso: nome_variavel = expressão
+
+# nome_completo = 'Paulo Carvalho'
+# int_um = int('1')
+# soma_dois_mais_dois = 2 + 2
+# print(int_um, type(int_um))
+# print(nome_completo, soma_dois_mais_dois)
+
+nome = 'Paulo'
+idade = 36
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'idade:', idade)
+print('É maior?', maior_de_idade)
