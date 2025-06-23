53303a7d3d4a7ee8e0a603f9aa3a1bc0a26bd8cb
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-06-23 13:14:48 -0300

    entendendo o fluxo do if elif e else, checando condições separadas

---
+++ 01_logica_programacao_basica/15_if_elif_else_entendendo_fluxo.py
@@ -0,0 +1,27 @@
+# if / elif       / else
+# if / se não     / se não
+
+condicao1 = False
+condicao2 = True
+condicao3 = True
+condicao4 = True
+
+
+if condicao1:
+    print('Código para condição 1')
+    print('Código para condição 1')
+elif condicao2:
+    print('Código para condição 2')
+elif condicao3:
+    print('Código para condição 3')
+elif condicao4:
+    print('Código para condição 4')
+else:
+    print('Nenhuma condição foi satisfetita!"')
+
+
+
+if 10 == 10:
+    print('outro if')
+
+print('Fora dos blocos ')
