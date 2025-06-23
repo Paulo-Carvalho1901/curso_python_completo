aaf3d615e5d8ddb9819ab5c4a84c73328dd69e18
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-06-22 21:09:15 -0300

    concluido exercicio de calculo de imc

---
+++ exercicios/02_calculo_imc.py
@@ -0,0 +1,10 @@
+print('Calculando o IMC')
+nome = 'Paulo'
+altura = 1.80
+peso = 96
+imc = peso / (altura * altura)
+# imc = peso / altura ** 2
+
+print(nome, 'tem', altura, 'de altura,')
+print('pesa', peso, 'quilos e seu IMC é')
+print(imc)
