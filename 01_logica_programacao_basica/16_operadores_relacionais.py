af8bebb461c07ad3cafeb964ebbe1769da502a77
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-06-23 14:07:57 -0300

    entendendo sobe operadores relacionais

---
+++ 01_logica_programacao_basica/16_operadores_relacionais.py
@@ -0,0 +1,22 @@
+# LINKS PARA ESTUDO
+# https://www.devmedia.com.br/operadores-no-python/40693
+# https://awari.com.br/operadores-relacionais-em-python-aprenda-tudo-sobre-eles/
+
+"""
+Operadores de comparação (relacionais)
+OP      Significado         Exemplo (True)
+>       maior               2 > 1
+>=      maior ou igual      2 >= 2
+<       menor               1 < 2
+<=      menor ou igual      2 <= 2
+==      igual               'a' == 'a'
+!=      diferente           'a' != 'b'
+"""
+
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 2
+menor_ou_igual = 2 <= 2
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+print(maior)
