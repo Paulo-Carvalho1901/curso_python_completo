e8ed8f9090af790fd5e217bcf318e5b873630f1a
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-07-06 04:18:10 -0300

    entendendo sobre ids como o Python identifica os valores na memoria

---
+++ 01_logica_programacao_basica/26_id_identidade_valor.py
@@ -0,0 +1,19 @@
+"""
+Flag (Bandeira) - Marca um local
+None = Não valor
+is e is not = é ou não é (tipo, valor, identidade)
+id = identidade
+
+"""
+
+v1 = 'a'
+v2 = 'a'
+print(id(v1))
+print(id(v2))
+
+# condição = False
+
+# if condicao:
+#   print('Faça algo')
+# else:
+#   print('Não faça algo')
