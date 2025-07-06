313e8a6eb64fb0f240c036ca01977fb6195aa685
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-07-06 03:53:05 -0300

    criando exercicico de variavel

---
+++ 01_logica_programacao_basica/25_variavel_constantates.py
@@ -0,0 +1,13 @@
+"""
+CONSTANTE = "Variáveis" que não vão mudar
+muitas codições no mesmo if (ruim)
+    <- contagem de complexidade (ruim)
+"""
+
+Velocidade = 61 # Velocidade atual de carro
+local_carro = 101 # local em que o carro está na estrada
+
+RADAR_1 = 60 # velocidade máxima do radar 1
+LOCAL_1 = 100 # local onde o radar 1 está
+RADAR_RANGE = 1 # A distância onde o radar pega
+
