06e63bb05a6c76cefab86b8ce68dde89c9a91565
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-07-07 15:17:32 -0300

    criado if para checar a condição

--- 01_logica_programacao_basica/26_id_identidade_valor.py
+++ 01_logica_programacao_basica/26_id_identidade_valor.py
@@ -21,5 +21,11 @@ if condicao:
 else:
   print('Não faça algo')
 
-print(passou_no_if, passou_no_if is None) # é None?
-print(passou_no_if, passou_no_if is not None) # Não é None?
+# print(passou_no_if, passou_no_if is None) # é None?
+# print(passou_no_if, passou_no_if is not None) # Não é None?
+
+if passou_no_if is None:
+  print('Não passou no if')
+
+if passou_no_if is not None:
+  print('Passou no if')
