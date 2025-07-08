88e9920c9a283c27bec36aec65d5d4dc50cffa5c
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-07-08 00:49:09 -0300

    estrutura condiconal exercicoo horario

--- exercicios/05_exercicios_3_em_1.py
+++ exercicios/05_exercicios_3_em_1.py
@@ -3,16 +3,16 @@ Faça um programa que peça ao usuário para digitar um número inteiro,
 informe se este número é par ou ímpar. Caso o usuário não digite um número
 inteiro, informe que não é um número inteiro.
 """
-number = input('Digite um número: ')
+# number = input('Digite um número: ')
 
-if number.isdigit():
-    number_int = int(number)
-    if number_int % 2 == 0:
-        print('Seu número é "PAR".')
-    else:
-        print('Seu número é "IMPAR".')
-else:
-    print('Você não digitou números inteiros.')
+# if number.isdigit():
+#     number_int = int(number)
+#     if number_int % 2 == 0:
+#         print('Seu número é "PAR".')
+#     else:
+#         print('Seu número é "IMPAR".')
+# else:
+#     print('Você não digitou números inteiros.')
 
 """
 Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
@@ -20,6 +20,18 @@ descrito, exiba a saudação apropriada. Ex.
 Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
 
+hora = input('Digite a hora desejada: ')
+
+if hora.isdigit():
+    hora_int = int(hora)
+    if hora_int >= 0 and hora_int <= 11:
+        print('Bom dia!')
+    elif hora_int >= 12 and hora_int <= 17:
+        print('Boa tarde!')
+    elif hora_int >= 18 and hora_int <= 23:
+        print('Boa noite')
+else:
+    print('Digite apenas números inteiros.')
 
 """
 Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
