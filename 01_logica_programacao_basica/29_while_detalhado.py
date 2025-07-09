278db575cfc907b02ee9f4cbe6c692275fcee592
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-07-09 03:05:36 -0300

    mais sobre while repetições

---
+++ 01_logica_programacao_basica/29_while_detalhado.py
@@ -0,0 +1,16 @@
+# While
+
+
+contador = 0 # criando condiçao
+cont = 0
+
+
+while contador <= 10: # verificando a condição
+
+    print(contador) # imprimindo contador
+    contador = contador + 1 # encrementando o contador
+
+
+
+
+print('Acabou!')
