0e174b1c97edd804a1952a1a4ddaf5fddc63d074
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-07-09 03:21:59 -0300

    iniciando com operadores de atribuição

---
+++ 01_logica_programacao_basica/30_operador_atribuicao.py
@@ -0,0 +1,13 @@
+# Operadores de atribuição
+# = += -= *= /= //= **=
+
+contador = 10
+
+# while contador <= 10:
+#     contador += 1
+#     print(contador)
+
+# print('FIM!')
+
+contador *= '2'
+print(contador)
