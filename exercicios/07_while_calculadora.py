3bca07d5602e45f8e29631c0ae8267826bd77f4c
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-07-11 03:17:10 -0300

    criado a logica dos numeros

--- exercicios/07_while_calculadora.py
+++ exercicios/07_while_calculadora.py
@@ -5,7 +5,20 @@ while True:
     numero_1 = input('Digite um número.')
     numero_2 = input('Digite um outro numero')
     operador = input('Digite o operador (+-/*): ')
+    
+    numero_validos = None # criado a flag
 
+    # Tratando os erros
+    try:
+        numero_1_float = float(numero_1)
+        numero_2_float = float(numero_2)
+        numero_validos = True
+    except:
+        numero_validos = None
+
+    if numero_validos is None:
+        print('Um ou ambos os números digitados são inválidos.')
+        continue
 
     # Validando campo sair
     sair = input('Quer sair? [s]im: ').lower().startswith('s')
