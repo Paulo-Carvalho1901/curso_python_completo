d0352ce217d2c34a4c4de99531e156462d5fdc6c
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-07-11 03:08:39 -0300

    criando as variaveis

--- exercicios/07_while_calculadora.py
+++ exercicios/07_while_calculadora.py
@@ -1,6 +1,13 @@
 # Exercicio calculadora
 
 while True:
+    # Solicitando campos ao usuário
+    numero_1 = input('Digite um número.')
+    numero_2 = input('Digite um outro numero')
+    operador = input('Digite o operador (+-/*): ')
+
+
+    # Validando campo sair
     sair = input('Quer sair? [s]im: ').lower().startswith('s')
     
     if sair is True:
