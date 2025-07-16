382e2b5f80f20df0055a04e3091730a03fc508f2
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-07-16 16:18:16 -0300

    inicializando com repetições for

--- 01_logica_programacao_basica/35_for_introducao.py
+++ 01_logica_programacao_basica/35_for_introducao.py
@@ -9,15 +9,24 @@
 
 #     i += 1
 
-senha_salva = '1234'
-senha_digitada = ''
-repeticaoes = 0
+# senha_salva = '1234'
+# senha_digitada = ''
+# repeticaoes = 0
 
 
-while senha_salva != senha_digitada:
-    senha_digitada = input(f'Sua senha ({repeticaoes}x): ')
+# while senha_salva != senha_digitada:
+#     senha_digitada = input(f'Sua senha ({repeticaoes}x): ')
 
-    repeticaoes += 1
+#     repeticaoes += 1
 
-print(repeticaoes)
-print('Aquele laço acima pode ter repetições infinitas.')
+# print(repeticaoes)
+# print('Aquele laço acima pode ter repetições infinitas.')
+
+
+texto = 'Python'
+
+novo_texto = ''
+for letra in texto:
+    novo_texto += f'*{letra}'
+    print(letra)
+print(novo_texto + '*')
