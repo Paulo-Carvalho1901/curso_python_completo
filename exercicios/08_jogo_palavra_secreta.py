970e377aeab6672a9d2e149a898e3e5700b45df7
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-07-17 15:55:57 -0300

    concluido programa jogo de palavra

--- exercicios/08_jogo_palavra_secreta.py
+++ exercicios/08_jogo_palavra_secreta.py
@@ -14,12 +14,15 @@ na palavra secreta.
 Faça a contagem de tentativas do seu
 usuário.
 """
+import os
 
 palavra_secreta = 'perfume'
 letras_acertadas = ''
+numero_tentativas = 0
 
 while True:
     letra_digitada = input('Digite uma letra: ')
+    numero_tentativas += 1
 
     # criado logica para ser digitado apenas uma letra
     if len(letra_digitada) > 1:
@@ -30,9 +33,20 @@ while True:
     if letra_digitada in palavra_secreta:
         letras_acertadas += letra_digitada
 
-    # criando logica para ver a letra_secreta dentro da palavra secreta
+    # criando logica para ver a palavra_formada dentro da palavra secreta
+    palavra_formada = ''
     for letra_secreta in palavra_secreta:
         if letra_secreta in letras_acertadas:
-            print(letra_secreta)
+            palavra_formada += letra_secreta
         else:
-            print('*')
+            palavra_formada += '*'
+
+    print('Palavra formada:', palavra_formada)
+
+    if palavra_formada == palavra_secreta:
+        os.system('cls')
+        print('VOCÊ GANHOU! PARABÉNS!')
+        print('A palavra era', palavra_secreta)
+        print('Tentativas', numero_tentativas)
+        letras_acertadas = ''
+        numero_tentativas = 0
