56344bef08d550985f24078b4161cdfd6297f111
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-07-21 14:28:06 -0300

    acvessando indices na lista e exibindo os indices e os valores

---
+++ exercicios/09_indeces_lista.py
@@ -0,0 +1,13 @@
+# Exercicos
+# Exbiba os índices da lista
+
+lista = ['Maria', 'Helena', 'Luiz']
+lista.append('João')
+lista.append('Davi')
+
+
+indices = range(len(lista))
+
+
+for indice in indices:
+    print(indice ,lista[indice])
