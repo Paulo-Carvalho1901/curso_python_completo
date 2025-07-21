db4112674a6d0d31971c6d96749b6b649fa0d2f9
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-07-21 14:11:35 -0300

    introdução a função copy das listas valores mutaveis

---
+++ 01_logica_programacao_basica/43_list_copy.py
@@ -0,0 +1,18 @@
+"""
+Cuidados com dados mutáveis
+= - copiado o valor (imutáveis)
+= - aponta para o mesmo valor na memória (mutável)
+"""
+
+# nome = 'Paulo'
+# outra_variavel = nome
+# nome = 'Andreia'
+# print(nome)
+# print(outra_variavel)
+
+lista_a = ['Luiz', 'Maria', 1, True, 1.2]
+lista_b = lista_a.copy()
+
+lista_a[0] = 'Qualquer coisa'
+print(lista_a)
+print(lista_b)
