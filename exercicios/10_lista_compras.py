3e22bb62defae4d449637ae8dcb584d2a8c41312
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-07-24 02:17:38 -0300

    criando o exercicio de lista de compra apresentando o problema

---
+++ exercicios/10_lista_compras.py
@@ -0,0 +1,8 @@
+'''
+Faça uma lista de compras com listas
+o usuario deve ter a possibilidade de
+inserir, apagar, e lista valores de sua lista
+não permita que o programa quebre com
+erros de indices inexistentes na lista
+'''
+
