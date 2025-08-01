afb046340dc682055049f9fa95a3ae60af46a3da
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-08-01 01:24:13 -0300

    criado as opções de if elif e else

--- exercicios/10_lista_compras.py
+++ exercicios/10_lista_compras.py
@@ -5,10 +5,19 @@ inserir, apagar, e lista valores de sua lista
 não permita que o programa quebre com
 erros de indices inexistentes na lista
 '''
+import os
 
 lista = []
 
 while True:
     print('Selecione uma opção')
     opcao = input('[i]nserir [a]pagar [l]istar: ')
-    
+
+    if opcao == 'i':
+        print('i')
+    elif opcao == 'a':
+        print('a')
+    elif opcao == 'l':
+        print('l')
+    else:
+        print('Por favor, escolha i, a ou l. ')
