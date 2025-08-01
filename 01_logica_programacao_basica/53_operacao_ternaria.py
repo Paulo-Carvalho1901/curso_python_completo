93bda800ee9d0fcca1f3cbee46a2fe71f3933eca
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-08-01 15:15:57 -0300

    mais exemplos de funções ternarias

--- 01_logica_programacao_basica/53_operacao_ternaria.py
+++ 01_logica_programacao_basica/53_operacao_ternaria.py
@@ -2,7 +2,11 @@
 Operação ternaria (condicional de uma linha)
 <valor> if <condicional> else <outro valor>
 """
-condicao = 10 == 11
-variavel = 'valor' if condicao else 'outro valor'
-print(variavel)
+# condicao = 10 == 11
+# variavel = 'valor' if condicao else 'outro valor'
+# print(variavel)
 
+digito = 9 # > 9 = 0
+# novo_digito = digito if digito <= 9 else 0
+novo_digito = 0 if digito > 9 else digito
+print(novo_digito)
