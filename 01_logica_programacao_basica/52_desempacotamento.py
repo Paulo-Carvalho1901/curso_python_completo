ec9e1c6210ee175106fe814233af529c9baa633a
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-08-01 15:04:18 -0300

    desempacotamento na chamada das função

--- 01_logica_programacao_basica/52_desempacotamento.py
+++ 01_logica_programacao_basica/52_desempacotamento.py
@@ -6,6 +6,26 @@ string = 'ABCD'
 lista = ['Maria', 'Helena', 1, 2, 3, 'Eduardo']
 tupla = 'Python', 'é', 'legal'
 
+salas = [
+    # 0        1
+    ['Maria', 'Helena', ],  # 0
+    # 0
+    ['Elaine', ],  # 1
+    # 0       1       2
+    ['Luiz', 'João', 'Eduarda', ],  # 2
+]
 
-p, b, *_, ap, u = lista
-print(p, u, ap)
+
+# p, b, *_, ap, u = lista
+# print(p, u, ap)
+
+# for nome in lista: 
+#     print(nome, end=' ')
+
+# Desempacotando
+# print('Maria', 'Helena', 1, 2, 3, 'Eduardo')
+# print(*lista)
+# print(*string)
+# print(*tupla)
+
+print(*salas, sep='\n')
