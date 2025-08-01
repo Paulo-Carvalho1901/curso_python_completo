d0cd22f10bccb443eaea70296fa4b5f58b1fae70
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-08-01 10:39:21 -0300

    funções como strip, split nas listas

---
+++ 01_logica_programacao_basica/49_split_join_strip.py
@@ -0,0 +1,16 @@
+"""
+split e join com list e str
+split - divide uma string (list)
+join - une uma string
+"""
+
+frase = '      Olha só que    ,      coisa interessante       '
+lista_frases_cruas = frase.split(',') # mantendo a lista original
+
+lista_frases = [] # alterando a lista com novas informaçoes alterando a lisa original
+for i, frase in enumerate(lista_frases_cruas):
+    lista_frases.append(lista_frases_cruas[i].strip())
+
+
+print(lista_frases_cruas)
+print(lista_frases)
