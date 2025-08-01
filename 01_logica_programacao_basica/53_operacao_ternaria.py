5338a5e4396a37e4a8ffd1fdc817c2689a888945
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-08-01 15:10:19 -0300

    introdução a condição ternaria

---
+++ 01_logica_programacao_basica/53_operacao_ternaria.py
@@ -0,0 +1,8 @@
+"""
+Operação ternaria (condicional de uma linha)
+<valor> if <condicional> else <outro valor>
+"""
+condicao = 10 == 11
+variavel = 'valor' if condicao else 'outro valor'
+print(variavel)
+
