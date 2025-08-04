5a35a10ac7f22dbb78ad17da33969aed6c22c5c6
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-08-04 16:06:27 -0300

    calculo segundo digito para cpf

--- exercicios/11_gerar_cpf.py
+++ exercicios/11_gerar_cpf.py
@@ -1,40 +1,42 @@
 """
-Calculo do primeiro dígito do CPF
+Calculo do segundo dígito do CPF
 CPF: 746.824.890-70
-Colete a soma dos 9 primeiros dígitos do CPF
+Colete a soma dos 9 primeiros dígitos do CPF,
+MAIS O PRIMEIRO DIGITO,
 multiplicando cada um dos valores por uma
-contagem regressiva começando de 10
+contagem regressiva começando de 11
 
-Ex.:  746.824.890-70 (746824890)
-   10  9  8  7  6  5  4  3  2
-*  7   4  6  8  2  4  8  9  0
-   70  36 48 56 12 20 32 27 0
+Ex.:  746.824.890-70 (7468248907)
+   11 10  9  8  7  6  5  4  3  2
+*  7   4  6  8  2  4  8  9  0  7 <-- PRIMEIRO DIGITO
+   77 40 54 64 14 24 40 36  0 14
 
-Somar todos os resultados: 
-70+36+48+56+12+20+32+27+0 = 301
+Somar todos os resultados:
+77+40+54+64+14+24+40+36+0+14 = 363
 Multiplicar o resultado anterior por 10
-301 * 10 = 3010
+363 * 10 = 3630
 Obter o resto da divisão da conta anterior por 11
-3010 % 11 = 7
+3630 % 11 = 0
 Se o resultado anterior for maior que 9:
     resultado é 0
 contrário disso:
     resultado é o valor da conta
 
-O primeiro dígito do CPF é 7
+O segundo dígito do CPF é 0
 """
-cpf = '74682489070' # cpf
-nove_digito = cpf[:9] # pegando os 9 primeiros digitos do cpf
+
+cpf_enviado_usuario = '74682489070' # cpf
+nove_digito = cpf_enviado_usuario[:9] # pegando os 9 primeiros digitos do cpf
 contador_regresivo_1 = 10 
 
 resultado_digito_1 = 0
-for digito_1 in nove_digito: # iterando em cada um dos 9 digito 
+for digito in nove_digito: # iterando em cada um dos 9 digito 
 
     """
     Efetuando a multiplicação do digito mais contador_regresivo
     concatenando com o resultando em uma soma
     """
-    resultado_digito_1 += int(digito_1) * contador_regresivo_1
+    resultado_digito_1 += int(digito) * contador_regresivo_1
     contador_regresivo_1 -= 1
 
 """
@@ -46,4 +48,22 @@ digito_1 = (resultado_digito_1 * 10) % 11
 
 # Aqui será feito uma operação ternaria
 digito_1 = digito_1 if digito_1 <= 9 else 0
-print(digito_1)
+
+
+dez_digitos = nove_digito + str(digito_1)
+contador_regresivo_2 = 11
+resultado_digito_2 = 0
+
+for digito in dez_digitos:
+    resultado_digito_2 += int(digito) * contador_regresivo_2
+    contador_regresivo_2 -= 1
+
+digito_2 = (resultado_digito_2 * 10) % 11
+
+cpf_gerado_pelo_calculo = f'{nove_digito}{digito_1}{digito_2}'
+
+if cpf_enviado_usuario == cpf_gerado_pelo_calculo:
+    print(f'{cpf_enviado_usuario} é valido.')
+else:
+    print('CPF invalido.')
+
