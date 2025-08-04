01270a9cde6ac33002b6b2c4c0ccfcab69281c43
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-08-04 16:55:45 -0300

    criado um gerador de cpf

--- exercicios/11_gerar_cpf.py
+++ exercicios/11_gerar_cpf.py
@@ -1,89 +1,29 @@
-"""
-Calculo do segundo dígito do CPF
-CPF: 746.824.890-70
-Colete a soma dos 9 primeiros dígitos do CPF,
-MAIS O PRIMEIRO DIGITO,
-multiplicando cada um dos valores por uma
-contagem regressiva começando de 11
+import random
 
-Ex.:  746.824.890-70 (7468248907)
-   11 10  9  8  7  6  5  4  3  2
-*  7   4  6  8  2  4  8  9  0  7 <-- PRIMEIRO DIGITO
-   77 40 54 64 14 24 40 36  0 14
+for _ in range(10):
+    nove_digito = ''
+    for i in range(9):
+        nove_digito += str(random.randint(0, 9))
 
-Somar todos os resultados:
-77+40+54+64+14+24+40+36+0+14 = 363
-Multiplicar o resultado anterior por 10
-363 * 10 = 3630
-Obter o resto da divisão da conta anterior por 11
-3630 % 11 = 0
-Se o resultado anterior for maior que 9:
-    resultado é 0
-contrário disso:
-    resultado é o valor da conta
+    contador_regresivo_1 = 10 
 
-O segundo dígito do CPF é 0
-"""
-import re
-import sys
+    resultado_digito_1 = 0
+    for digito in nove_digito:
+        resultado_digito_1 += int(digito) * contador_regresivo_1
+        contador_regresivo_1 -= 1
+    digito_1 = (resultado_digito_1 * 10) % 11 
+    digito_1 = digito_1 if digito_1 <= 9 else 0
 
-# cpf_enviado_usuario = '746.824.890-70'\
-#     .replace('.', '') \
-#     .replace(' ', '') \
-#     .replace('-', '')
-    # cpf
+    dez_digitos = nove_digito + str(digito_1)
+    contador_regresivo_2 = 11
+    resultado_digito_2 = 0
 
-entrada = input('CPF: [746.824.890-70]: ')
-cpf_enviado_usuario = re.sub(
-    r'[^0-9]',
-    '',
-    entrada
-)
+    for digito in dez_digitos:
+        resultado_digito_2 += int(digito) * contador_regresivo_2
+        contador_regresivo_2 -= 1
+    digito_2 = (resultado_digito_2 * 10) % 11
 
-entrada_e_sequencial = entrada == entrada[0] * len(entrada)
+    cpf_gerado_pelo_calculo = f'{nove_digito}{digito_1}{digito_2}'
 
-if entrada_e_sequencial:
-    print('Você enviou dados sequenciais.')
-    sys.exit()
-
-nove_digito = cpf_enviado_usuario[:9] # pegando os 9 primeiros digitos do cpf
-contador_regresivo_1 = 10 
-
-resultado_digito_1 = 0
-for digito in nove_digito: # iterando em cada um dos 9 digito 
-
-    """
-    Efetuando a multiplicação do digito mais contador_regresivo
-    concatenando com o resultando em uma soma
-    """
-    resultado_digito_1 += int(digito) * contador_regresivo_1
-    contador_regresivo_1 -= 1
-
-"""
-resultado * 10 sera executado primeiro, depois
-será executado o resto da divisão
-
-"""
-digito_1 = (resultado_digito_1 * 10) % 11 
-
-# Aqui será feito uma operação ternaria
-digito_1 = digito_1 if digito_1 <= 9 else 0
-
-
-dez_digitos = nove_digito + str(digito_1)
-contador_regresivo_2 = 11
-resultado_digito_2 = 0
-
-for digito in dez_digitos:
-    resultado_digito_2 += int(digito) * contador_regresivo_2
-    contador_regresivo_2 -= 1
-
-digito_2 = (resultado_digito_2 * 10) % 11
-
-cpf_gerado_pelo_calculo = f'{nove_digito}{digito_1}{digito_2}'
-
-if cpf_enviado_usuario == cpf_gerado_pelo_calculo:
-    print(f'{cpf_enviado_usuario} é valido.')
-else:
-    print('CPF invalido.')
-
+    print(cpf_gerado_pelo_calculo)
+    
