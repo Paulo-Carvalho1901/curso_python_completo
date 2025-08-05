121064491aad950c9fe5ffe5951180b360956b4d
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-08-05 13:55:24 -0300

    teste 06 sobre argumentos nomeados e argumentos posicionais em funções

---
+++ teste_seus_conhecimentos/teste_06/01.py
@@ -0,0 +1,14 @@
+"""
+Quando falamos em argumentos, estamos falando sobre os valores passados para as funções no ato da
+sua execução. Existem argumentos nomeados e argumentos posicionais.
+
+Argumentos nomeados recebem o nome do parâmetro antes do valor, argumentos posicionais recebem
+apenas o valor para preencher o parâmetro na ordem.
+
+Por qual motivo você usaria argumentos nomeados?
+
+Resposta:
+É interresante usar argumentos nomeados poder alterar a ordem no envio de
+valores para função.
+
+"""
---
+++ teste_seus_conhecimentos/teste_06/02.py
@@ -0,0 +1,7 @@
+"""
+A regra é clara:
+
+Resposta:
+Você não pode enviar augumentos posicionais após argumentos nomeados
+
+"""
