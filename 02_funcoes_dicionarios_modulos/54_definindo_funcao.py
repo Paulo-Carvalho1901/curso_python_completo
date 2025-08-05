3a3859684ff45d4b8ee34c9603f6e54930697f71
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-08-05 11:22:13 -0300

    introducao a funções e sua sintax

---
+++ 02_funcoes_dicionarios_modulos/54_definindo_funcao.py
@@ -0,0 +1,30 @@
+"""
+Introdução às funções (def) en Python
+Funções são trechos de códigos usados para
+replicar determinada ação ao longo do seu código
+Eles podem receber valores por parâmetros (argumentos)
+e retornar um valor especifico.
+por padrão, funções Python retornam None (nada)
+"""
+
+# def Python(a, b, c):
+#     print('Varias1')
+#     print('Varias2')
+#     print('Varias3')
+#     print('Varias4')
+
+# def imprimir(a, b, c): # parametros
+#     print(a, b, c)
+
+
+
+# imprimir(1, 2, 3) # Argumentos
+# imprimir(4, 5, 6) 
+
+def saudacao(nome):
+    print(f'Bem vindo! {nome}')
+
+
+saudacao('Paulo Carvalho')
+saudacao('Andreia Carvalho')
+saudacao('Davi Carvalho')
