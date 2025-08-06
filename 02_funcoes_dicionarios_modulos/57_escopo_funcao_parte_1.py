13f45e62404a4fd0745fa34d0f189b0e00613018
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-08-06 15:20:34 -0300

    executando varios escopos de funções, com suas respectivas variaveis do seu escopo

--- 02_funcoes_dicionarios_modulos/57_escopo_funcao_parte_1.py
+++ 02_funcoes_dicionarios_modulos/57_escopo_funcao_parte_1.py
@@ -7,9 +7,20 @@ O escopo local é o escopo onde apenas nomes do mesmo local
 podem ser alcançados.
 """
 
+
+x = 1
+
 def escopo():
-    x = 1
+    x = 10
+
+    def outra_funcao():
+        x = 11
+        y = 2
+        print(x, y)
+
+    outra_funcao()
     print(x)
 
-
+print(x)
 escopo()
+print(x)
