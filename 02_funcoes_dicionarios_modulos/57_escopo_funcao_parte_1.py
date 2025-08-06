aea8e48444cc9d2bf535e06dd5257ea35f669317
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-08-06 16:38:50 -0300

    interagindo com escopo da função

--- 02_funcoes_dicionarios_modulos/57_escopo_funcao_parte_1.py
+++ 02_funcoes_dicionarios_modulos/57_escopo_funcao_parte_1.py
@@ -16,18 +16,18 @@ x = 1
 
 
 def escopo():
-    global x
-    x = 10
+    #global x
+    #x = 10
 
     def outra_funcao():
-        global x
-        x = 11
+        #global x
+        #x = 11
         y = 2
         print(x, y)
 
     outra_funcao()
-    print(x)
+    #print(x)
 
-print(x)
+#print(x)
 escopo()
-print(x)
+#print(x)
