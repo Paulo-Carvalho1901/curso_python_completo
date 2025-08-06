6a2cc78c7bb5fdc0fd941a4557bc4322e1bc5a62
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-08-06 17:24:59 -0300

    finalizando conceitos basicos escopo de função

--- 02_funcoes_dicionarios_modulos/57_escopo_funcao_parte_1.py
+++ 02_funcoes_dicionarios_modulos/57_escopo_funcao_parte_1.py
@@ -17,17 +17,17 @@ x = 1
 
 def escopo():
     #global x
-    #x = 10
+    x = 10
 
     def outra_funcao():
         #global x
-        #x = 11
+        x = 11
         y = 2
         print(x, y)
 
     outra_funcao()
     #print(x)
 
-#print(x)
+print(x)
 escopo()
-#print(x)
+print(x)
