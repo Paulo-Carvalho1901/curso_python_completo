31473b37496a064287677a3f17fa3464985099b3
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-08-12 22:16:43 -0300

    finalizado closure em funções

--- 02_funcoes_dicionarios_modulos/61_closure_function.py
+++ 02_funcoes_dicionarios_modulos/61_closure_function.py
@@ -1,14 +1,17 @@
 # Closure e funções que retornan outras funções
 
-def criar_saudacao(saudacao, nome):
-    def saudar():
+def criar_saudacao(saudacao):
+    def saudar(nome):
         return f'{saudacao}, {nome}!'
     return saudar
 
 
+falar_bom_dia = criar_saudacao('Bom dia')
+falar_boa_noite = criar_saudacao('Bom noite')
 
-s1 = criar_saudacao('Bom dia', 'Paulo')
-s2 = criar_saudacao('Bom noite', 'Paulo')
+# print(falar_bom_dia('Paulo'))
+# print(falar_boa_noite('Paulo'))
 
-print(s1())
-print(s2())
+for nome in ['Paulo', 'Davi', 'Maria']:
+    print(falar_bom_dia(nome))
+    print(falar_boa_noite(nome))
