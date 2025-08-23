f9613a908bca9e76616997eb407089a9aa1fbc3c
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-08-23 00:39:30 -0300

    detalhando o uso de namespaces e escopo de funções

--- 02_funcoes_dicionarios_modulos/62_escopo_namespace.py
+++ 02_funcoes_dicionarios_modulos/62_escopo_namespace.py
@@ -30,6 +30,26 @@
 # Cada escopo tem seu "espaço de nomes" (namespace), que é um local onde os
 # nomes e seus respectivos objetos são armazenados
 
+##################################################################################
+
+# O que é namespace?
+
+#Namaspace é um estrutura de dados real que mapeia names para objetos. Cada
+# chave é o nome que você define e o valor é o objeto correspondente no seu
+# código. Sempre que você criar um name, essa associação é guardada dentro de um 
+# namespace
+# 
+# Vamos usar glabals() e locals() no mesmo código anterior e confirmar isso
+# 
+# globals(): Retorna um dicionario que representa o namespace global no módulo
+#            atual. isso inclui todos os nomes definidos na raiz do arquivo.
+# locals():  Retorna um dicionario com os nomes definidos no escopo local onde
+#            a função está sendo executada. Importante: ela só inclui nomes que 
+#            já forem definidos antes da sua chamada.
+
+#######################################################################################
+
+
 # nome definido no escopo global (módulo)
 um_nome = "um_nome (GLOBAL)"
 
@@ -43,4 +63,19 @@ def func_global(sou_local: str) -> None:
 
     # Parâmetros de funções também são de escopo local no função
     print(f'Dentro da função: {um_nome}, {outro_nome}, {sou_local}')
-    
+
+
+# ISSO NÃO FUNCIONA
+# print(outro_nome, sou_local)
+
+# Isso é injetado automaticamente pelo Python no escopo Global
+print('Nome do módulo:', __name__)
+print('Arquivo do módulo:', __file__)
+print('Documentação do módulo:', __doc__)
+print() # apenas uma quebra de linha
+
+func_global('arg (local)')
+# saída - Dentro da função: um_nome (LOCAL), outro_nome (LOCAL), org (local)
+
+print(f'Fora da função: {um_nome}') # Acessa a variável GLOBAL
+# saída - Fora da função: um_nome (GLOBAL)
