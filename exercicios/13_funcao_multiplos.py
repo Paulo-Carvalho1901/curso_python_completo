8afd8697d4cbfd542e5f113a277f6a5b9d6556ef
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-08-26 17:53:30 -0300

    uma possivel solução

---
+++ exercicios/13_funcao_multiplos.py
@@ -0,0 +1,20 @@
+# Exercicio
+# Crie funções que duplique, triplique, e quadruplique
+# o número recebido com paramtros
+
+# def duplicar(numero):
+#     return numero * 2
+
+# def triplicar(numero):
+#     return numero * 3
+
+# def quadruplica(numero):
+#     return numero * 4
+
+
+# print(duplicar(2))
+# print(triplicar(2))
+# print(quadruplica(2))
+
+
+
