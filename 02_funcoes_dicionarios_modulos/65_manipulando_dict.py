21276d43af5f40c4e203c2179d32d930f57b9525
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-08-26 22:06:13 -0300

    manipulando dicionarios CRUD

--- 02_funcoes_dicionarios_modulos/65_manipulando_dict.py
+++ 02_funcoes_dicionarios_modulos/65_manipulando_dict.py
@@ -3,8 +3,26 @@
 pessoa = {}
 
 # add algo em um dicionário vazio
-pessoa['nome'] = 'Paulo Carvalho'
-print(pessoa)
+chave = 'nome'
+pessoa[chave] = 'Paulo Carvalho'
+
+# add chave sobrenome com valor
+pessoa['sobrenome'] = 'Carvalho'
+# print(pessoa[chave])
+
+# reatribuindo a chave a outro valor 'rescrevendo o valor'
+pessoa[chave] = 'Davi'
+
+# apagando chave do dicionário
+del pessoa['sobrenome']
+print(pessoa['nome'])
+
+# print(pessoa.get('sobrenome'))
+
+if pessoa.get('sobrenome') is None:
+    print('NÃO EXISTE!')
+else:
+    print(pessoa['sobrenome'])
 
 # acessando o valor pela chave do dicionário
-print(pessoa['nome'])
+# print(pessoa['nome'])
