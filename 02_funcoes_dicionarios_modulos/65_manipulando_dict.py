78f7bff6d195118c5feda03f74b313f944394bd0
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-08-26 21:49:11 -0300

    manipulando chaves e valores em um dicionário

---
+++ 02_funcoes_dicionarios_modulos/65_manipulando_dict.py
@@ -0,0 +1,10 @@
+# Manipulando chaves e valores em dicionários
+
+pessoa = {}
+
+# add algo em um dicionário vazio
+pessoa['nome'] = 'Paulo Carvalho'
+print(pessoa)
+
+# acessando o valor pela chave do dicionário
+print(pessoa['nome'])
