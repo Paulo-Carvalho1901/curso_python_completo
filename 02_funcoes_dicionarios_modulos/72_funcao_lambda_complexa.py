17bfb4bfc5dcc7e7f895901b738fda2cf8ee950a
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-11 22:01:17 -0300

    substituindo funções por funções lambda

--- 02_funcoes_dicionarios_modulos/72_funcao_lambda_complexa.py
+++ 02_funcoes_dicionarios_modulos/72_funcao_lambda_complexa.py
@@ -3,15 +3,23 @@ def executa(funcao, *args):
 
 
 
-def soma(x, y):
-    return x + y
+# def soma(x, y):
+#     return x + y
 
 
 
-def cria_multiplicador(multiplicador):
-    def multiplica(numero):
-        return numero * multiplicador
-    return multiplica
+# def cria_multiplicador(multiplicador):
+#     def multiplica(numero):
+#         return numero * multiplicador
+#     return multiplica
+
+
+# duplica = cria_multiplicador(2)
+duplica = executa(
+    lambda m: lambda n: n * m,
+    2 
+)
+print(duplica(2))
 
 
 print(
@@ -19,6 +27,11 @@ print(
         lambda x, y: x + y,
         2, 3 
     ),
-    executa(soma, 2, 3),
-    soma(2, 3)
+)
+
+print(
+    executa(
+         lambda *args: sum(args),
+         2, 2, 45, 1, 2
+    )
 )
