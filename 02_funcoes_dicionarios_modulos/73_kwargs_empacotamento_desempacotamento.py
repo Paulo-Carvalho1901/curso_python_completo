c7ce3482f31628d4a037ae4e9b651551b1029b66
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-11 22:06:16 -0300

    começando com empacotamento

---
+++ 02_funcoes_dicionarios_modulos/73_kwargs_empacotamento_desempacotamento.py
@@ -0,0 +1,15 @@
+# Empacotamento e desempacotamento de dicionarios
+
+a, b = 1, 2
+a, b = b, a
+print(a, b)
+
+
+pessoa = {
+    'nome': 'Aline',
+    'sobrenome': 'Souza',
+}
+
+# args e Kwargs
+# args (já vimos)
+# kwargs - keyword arguments (argumentos nomeados)
