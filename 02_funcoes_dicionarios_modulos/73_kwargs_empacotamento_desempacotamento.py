924c82529b99bf7d6c3477af738317bcf72a5a84
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-11 22:15:42 -0300

    demostrando varios tipos de desempacotamento

--- 02_funcoes_dicionarios_modulos/73_kwargs_empacotamento_desempacotamento.py
+++ 02_funcoes_dicionarios_modulos/73_kwargs_empacotamento_desempacotamento.py
@@ -2,13 +2,27 @@
 
 a, b = 1, 2
 a, b = b, a
-print(a, b)
+# print(a, b)
 
 
 pessoa = {
     'nome': 'Aline',
     'sobrenome': 'Souza',
 }
+
+# desempacotamento
+# a, b = pessoa.values()
+# # print(a, b)
+
+# # desempacotamento interno
+# (a1, a2), (b1, b2) = pessoa.items()
+# print(a1, a2)
+# print(b1, b2)
+
+
+# # itenrando com for
+# for chave, valor in pessoa.items():
+#     print(chave, valor) # retorna uma tupla com valor
 
 # args e Kwargs
 # args (já vimos)
