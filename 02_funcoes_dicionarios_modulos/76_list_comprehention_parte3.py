718fc54adf0b8edf4fa9777c00925fab87df4001
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-25 19:38:19 -0300

    criado função para operação operação de list comprehenrtion

--- 02_funcoes_dicionarios_modulos/76_list_comprehention_parte3.py
+++ 02_funcoes_dicionarios_modulos/76_list_comprehention_parte3.py
@@ -1,7 +1,19 @@
-lista_original = [1, 2, 3, 4, 5] # criando uma lista de números
+def divisaoFn(x, y):
+    return x / y
 
+
+def multiplicacaoFn(x, y):
+    return x * y
+
+
+def potenciacaoFn(x, y):
+    return x ** y
+
+lista_original = [1, 2, 3, 4, 5] # criando uma lista de números
 # Criando uma nova variavel e copiando o valor com list comprehention
-divisao = [numero / 2 for numero in lista_original] # mapeamento de lista para outra lista
+divisao = [divisaoFn(numero, 2) for numero in lista_original] # mapeamento de lista para outra lista
+multiplicacao = [multiplicacaoFn(numero, 2) for numero in lista_original] # mapeamento de lista para outra lista
+quadrados = [potenciacaoFn(numero, 2) for numero in lista_original] # mapeamento de lista para outra lista
 
 # Utilizando o for, o mesmo que tilizando list comprehention
 # novos_numeros = []
@@ -11,5 +23,7 @@ divisao = [numero / 2 for numero in lista_original] # mapeamento de lista para o
 # modificando a variavel numeros, indice 0, novo_valor = 20
 
 print('lista original', lista_original) # aqui a variavel permanecerá com os valores iniciais
-print('lista alterada', divisao) # aqui a variavel tera seu valor alterado
+print('lista divisao', divisao) # aqui a variavel tera seu valor alterado
+print('lista multiplicacao', multiplicacao) # aqui a variavel tera seu valor alterado
+print('lista quadrados', quadrados) # aqui a variavel tera seu valor alterado
 
