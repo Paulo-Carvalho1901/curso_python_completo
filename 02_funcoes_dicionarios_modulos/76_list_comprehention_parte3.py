e27c23ab432c749bf1dd87f1c90c0bbe68c77f76
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-25 19:27:51 -0300

    mapeamento de para uma outra lista

--- 02_funcoes_dicionarios_modulos/76_list_comprehention_parte3.py
+++ 02_funcoes_dicionarios_modulos/76_list_comprehention_parte3.py
@@ -1,15 +1,15 @@
-numeros = [1, 2, 3, 4, 5] # criando uma lista de números
+lista_original = [1, 2, 3, 4, 5] # criando uma lista de números
 
 # Criando uma nova variavel e copiando o valor com list comprehention
-# novos_numeros = [numero for numero in numeros]
+divisao = [numero / 2 for numero in lista_original] # mapeamento de lista para outra lista
 
 # Utilizando o for, o mesmo que tilizando list comprehention
-novos_numeros = []
-for numero in numeros:
-    novos_numeros.append(numero)
+# novos_numeros = []
+# for numero in numeros:
+#     novos_numeros.append(numero)
 
 # modificando a variavel numeros, indice 0, novo_valor = 20
-numeros[0] = 20
-print('numeros', numeros) # aqui a variavel tera seu valor alterado
-print('novos_numeros', novos_numeros) # aqui a variavel permanecerá com os valores iniciais
+
+print('lista original', lista_original) # aqui a variavel permanecerá com os valores iniciais
+print('lista alterada', divisao) # aqui a variavel tera seu valor alterado
 
