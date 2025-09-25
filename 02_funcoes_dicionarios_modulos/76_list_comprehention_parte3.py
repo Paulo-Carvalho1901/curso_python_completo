459b9deccaf6430d4aa1145f2cb033bdd865ce88
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-25 18:36:52 -0300

    agora utilizando o for para fazer a mesma copia com list comprehention

--- 02_funcoes_dicionarios_modulos/76_list_comprehention_parte3.py
+++ 02_funcoes_dicionarios_modulos/76_list_comprehention_parte3.py
@@ -1,7 +1,12 @@
 numeros = [1, 2, 3, 4, 5] # criando uma lista de números
 
 # Criando uma nova variavel e copiando o valor com list comprehention
-novos_numeros = [numero for numero in numeros]
+# novos_numeros = [numero for numero in numeros]
+
+# Utilizando o for, o mesmo que tilizando list comprehention
+novos_numeros = []
+for numero in numeros:
+    novos_numeros.append(numero)
 
 # modificando a variavel numeros, indice 0, novo_valor = 20
 numeros[0] = 20
