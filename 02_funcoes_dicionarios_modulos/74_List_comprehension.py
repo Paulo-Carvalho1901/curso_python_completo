2fbc355be1487d99eb42cf15705a206da06559ee
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-24 22:29:22 -0300

    criando uma funcao para pprint e colocado filtro em uma list comprehension

--- 02_funcoes_dicionarios_modulos/74_List_comprehension.py
+++ 02_funcoes_dicionarios_modulos/74_List_comprehension.py
@@ -2,6 +2,11 @@
 # List comprehension é uma form rápida para criar listas
 # a partir de iteráveis
 
+import pprint
+
+def p(v):
+    pprint.pprint(v, sort_dicts=False, width=40)
+
 # print(list(range(10)))
 
 lista = []
@@ -20,12 +25,16 @@ produtos = [
     {'nome': 'p2', 'preco': 10, },
     {'nome': 'p3', 'preco': 30, },
 ]
-
-
 novos_produtos = [
     {**produto, 'preco': produto['preco'] * 1.05} 
     if produto['preco'] > 20 else {**produto}
     for produto in produtos
 ]
 # print(novos_produtos)
-print(*novos_produtos, sep='\n')
+# print(*novos_produtos, sep='\n')
+# p(novos_produtos)
+
+
+# Filtro
+lista = [n for n in range(10) if n < 5]
+print(lista)
