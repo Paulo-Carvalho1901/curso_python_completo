62f7258e466bae6a32ad8d5a29c418d2f9abda73
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-26 08:55:07 -0300

    filtro em list comprehention

--- 02_funcoes_dicionarios_modulos/76_list_comprehention_parte3.py
+++ 02_funcoes_dicionarios_modulos/76_list_comprehention_parte3.py
@@ -27,3 +27,20 @@ print('lista divisao', divisao) # aqui a variavel tera seu valor alterado
 print('lista multiplicacao', multiplicacao) # aqui a variavel tera seu valor alterado
 print('lista quadrados', quadrados) # aqui a variavel tera seu valor alterado
 
+print('=-' * 30)
+print()
+
+# Filter
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+novos_numeros = [numero for numero in numeros if numero > 5] # filter
+impares = [numero for numero in numeros if numero % 2 != 0] # filter
+pares = [numero for numero in numeros if numero % 2 == 0] # filter
+outro_if = [numero if numero != 6 else 600 for numero in numeros if numero % 2 == 0]
+
+
+print(numeros)
+print(novos_numeros)
+print(impares)
+print(pares)
+print(outro_if)
