6b37395cb8ed6e058372a771a5ca1dfa5a396c7e
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-26 15:12:03 -0300

    for aninhados com list comprehention

--- 02_funcoes_dicionarios_modulos/76_list_comprehention_parte3.py
+++ 02_funcoes_dicionarios_modulos/76_list_comprehention_parte3.py
@@ -53,3 +53,14 @@ for x in range(1, 4):
     for y in range(1, 4):
         print(x, y)
 
+print('=-' * 30)
+linhas_e_colunas = [
+    (x, y)
+    # if y == 2 else 'você nao é 2'
+    if y != 2 else (x, y * 1000)
+    for x in range(1, 4)
+    for y in range(1, 4)
+    # if x != 2
+]
+
+print(linhas_e_colunas)
