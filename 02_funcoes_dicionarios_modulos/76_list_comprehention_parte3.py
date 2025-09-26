da33521ad633d4bb2f54cdc322a7b691628a23fa
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-26 15:36:19 -0300

    list comprehention flat for aninhados

--- 02_funcoes_dicionarios_modulos/76_list_comprehention_parte3.py
+++ 02_funcoes_dicionarios_modulos/76_list_comprehention_parte3.py
@@ -82,3 +82,12 @@ print('Lista de nomes')
 nomes = ['luiz', 'maria', 'helena', 'joana', 'felipe']
 novos_nomes = [f'{nome[:-1].lower()}{nome[-1].upper()}' for nome in nomes]
 print(novos_nomes)
+
+
+print('=-' * 30)
+print('Lista flat')
+
+numeros = [[numero, numero ** 2] for numero in range(10)]
+flat = [y for x in numeros for y in x]
+print(numeros)
+print(flat)
