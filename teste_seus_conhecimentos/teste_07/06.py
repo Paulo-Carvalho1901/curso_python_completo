6798ce5890f18c691bb36bad3c25bcbfb9b12072
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-27 01:38:05 -0300

    introdução a deepcopy copia profunda dos seus itens

--- teste_seus_conhecimentos/teste_07/06.py
+++ teste_seus_conhecimentos/teste_07/06.py
@@ -1,7 +1,11 @@
-pessoas = ['Luiz', 'Maria', 'João']
-copia_de_pessoas = pessoas.copy() # shallow copy
+import copy
 
+pessoas = ['Luiz', 'Maria', 'João', ['outras']]
+# copia_de_pessoas = pessoas.copy() # shallow copy
+copia_de_pessoas = copy.deepcopy(pessoas) # deep copy
 
 copia_de_pessoas[0] = 'Paulo'
+copia_de_pessoas[3][0] = 'Andreia'
 print(pessoas)
 print(copia_de_pessoas)
+
