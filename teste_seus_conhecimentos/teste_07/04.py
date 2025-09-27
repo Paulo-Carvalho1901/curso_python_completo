0b2c4b6f9a9b7e62344d7573d4221ced0f3e5b47
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-27 01:10:57 -0300

    feito ordenaçao de uma lista de dict com uma função sort

---
+++ teste_seus_conhecimentos/teste_07/04.py
@@ -0,0 +1,28 @@
+# Introdução à função lambda (função anônima de uma linha)
+# A função lambda é uma função como qualquer
+# outra em Python. Porém, são funções anônimas
+# que contém apenas uma linha. Ou seja, tudo
+# deve ser contido dentro de uma única
+# expressão.
+
+# lista = [4, 32,1,34,5,6,6,2]
+# lista.sort(reverse=True)
+# # sorted(lista)
+# print(lista)
+
+lista = [
+    {'nome': 'Luiz', 'sobrenome': 'miranda'},
+    {'nome': 'Maria', 'sobrenome': 'Oliveira'},
+    {'nome': 'Daniel', 'sobrenome': 'Silva'},
+    {'nome': 'Eduardo', 'sobrenome': 'Moreira'},
+    {'nome': 'Aline', 'sobrenome': 'Souza'},
+]
+
+def ordena(item):
+    return item['nome']
+
+
+lista.sort(key=ordena)
+
+for item in lista:
+    print(item)
