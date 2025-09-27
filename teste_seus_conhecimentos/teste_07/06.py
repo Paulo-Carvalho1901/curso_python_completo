c0d933f4ce6f7558518593f6259babe76b91ab57
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-27 01:52:15 -0300

    mais um exemplo de shallo copy e deepcopy

--- teste_seus_conhecimentos/teste_07/06.py
+++ teste_seus_conhecimentos/teste_07/06.py
@@ -9,3 +9,21 @@ copia_de_pessoas[3][0] = 'Andreia'
 print(pessoas)
 print(copia_de_pessoas)
 
+names = {
+    'firstname': 'Paulo',
+    'lastname': 'Carvalho',
+    'adresses': [
+        {'linha1': 'AV Brasil', 'linha2': 30},
+        {'linha1': 'Rua Teste', 'linha2': 15},
+    ]
+}
+
+# copy_of_names = names.copy() # shallow
+copy_of_names = copy.deepcopy(names) # deepcopy
+
+
+copy_of_names['firstname'] = 'Andreia'
+copy_of_names['adresses'][1]['linha1'] = 'Mudei'
+copy_of_names['adresses'][1]['linha2'] = 33
+print(names)
+print(copy_of_names)
