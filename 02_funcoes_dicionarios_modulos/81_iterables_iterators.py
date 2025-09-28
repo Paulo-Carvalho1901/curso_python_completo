6670a48f263fe2b591bdc00db97e75a7ac362c57
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-28 00:28:54 -0300

    conceitos de next e iter nos iteraveis

--- 02_funcoes_dicionarios_modulos/81_iterables_iterators.py
+++ 02_funcoes_dicionarios_modulos/81_iterables_iterators.py
@@ -1,4 +1,8 @@
 # Generator expression, iterable e iterators em Python
 
 iterable = ['Eu', 'Tenho', '__iter__']
-iterator = iterable.__iter__() # tem __iter__ e __next__
+iterator = iter(iterable) # tem __iter__ e __next__
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator)) # StopIteration
