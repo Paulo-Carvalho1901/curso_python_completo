a8579044b0ffb952742d7d4659b8066365042f96
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-28 00:40:48 -0300

    diferenças entre generator e iterator

--- 02_funcoes_dicionarios_modulos/81_iterables_iterators.py
+++ 02_funcoes_dicionarios_modulos/81_iterables_iterators.py
@@ -1,8 +1,14 @@
 # Generator expression, iterable e iterators em Python
 
+import sys
+
 iterable = ['Eu', 'Tenho', '__iter__']
 iterator = iter(iterable) # tem __iter__ e __next__
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
-print(next(iterator)) # StopIteration
+lista = [n for n in range(10000)] # lista ja está todo na memoria
+generator = (n for n in range(10)) # esta no primeiro ponto do generator, não esta na memoria
+
+print(sys.getsizeof(lista)) # verifcando o tamanho em bytes
+print(sys.getsizeof(generator))
+
+# for n in generator:
+#     print(n)
