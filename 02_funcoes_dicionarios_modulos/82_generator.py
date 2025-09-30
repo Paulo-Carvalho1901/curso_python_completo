80cae7baf2ebf31d579691dd0c106c276ae905f6
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-30 00:53:49 -0300

    generator function que pausa

--- 02_funcoes_dicionarios_modulos/82_generator.py
+++ 02_funcoes_dicionarios_modulos/82_generator.py
@@ -1,18 +1,25 @@
 # Introdução às Generator functions em Python
 # generator = (n for n in range(10000))
 
-def generator(n=0):
-    yield 1 # pausar
-    print('continuando...')
-    yield 2 # pausar
-    print('mais um...')
-    yield 3 # pausar
-    print('vou terminar')
-    return 'fim'
+def generator(n=0, max=10):
+    while True:
+        yield n
+        n += 1
+
+        if n >= max:
+            return
+
+    # yield 1 # pausar
+    # print('continuando...')
+    # yield 2 # pausar
+    # print('mais um...')
+    # yield 3 # pausar
+    # print('vou terminar')
+    # return 'fim'
 
 
-gen = generator(n=0)
-print(gen.__iter__())
+gen = generator(n=5, max=15)
+# print(gen.__iter__())
 # print(next(gen))
 # print(next(gen))
 # print(next(gen))
