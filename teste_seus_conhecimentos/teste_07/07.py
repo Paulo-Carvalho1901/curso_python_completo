7f5105f681d6039d966c4bead06102a088af8fc0
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-09-30 12:01:54 -0300

    alterando valor de x dentro do escopo da outra_funcao x = 11

--- teste_seus_conhecimentos/teste_07/07.py
+++ teste_seus_conhecimentos/teste_07/07.py
@@ -12,13 +12,15 @@ x = 1
 
 def escopo():
     x = 10
-    
+
     def outra_funcao():
+        x = 11
         y = 2
         print(x, y)
 
     outra_funcao()
     print(x)
 
-
-escopo()
+print(x) # valor x do modulo principal
+escopo() 
+print(x) # valor x do modulo principal
