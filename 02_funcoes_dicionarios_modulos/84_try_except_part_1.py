7df906e01e023df72e6cb272098ab2e226f418d5
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-01 00:46:29 -0300

    tratando outros tipos de erro

--- 02_funcoes_dicionarios_modulos/84_try_except_part_1.py
+++ 02_funcoes_dicionarios_modulos/84_try_except_part_1.py
@@ -5,14 +5,19 @@
 
 try:
     a = 18
-    # b = 0
-    print('linha 1')
+    b = 0
+    # print(b[0])
+    print('linha 1'[100])
     c = a / b
     print('linha 2')
 except ZeroDivisionError:
     print('ERRO: Dividiu por zero')
 except NameError:
     print('ERRO: is not defined')
+except (TypeError, IndexError):
+    print('ERRO: erro de tipo')
+except Exception:
+    print('ERRO: Desconhecido')
 
 
 print('continuar...')
