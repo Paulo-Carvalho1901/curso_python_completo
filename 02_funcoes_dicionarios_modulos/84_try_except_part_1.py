c32bd316fa701763ede17e7569624e3122f705b6
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-01 00:51:47 -0300

    colocando algumas explicações no codigo para melhor entendimento

--- 02_funcoes_dicionarios_modulos/84_try_except_part_1.py
+++ 02_funcoes_dicionarios_modulos/84_try_except_part_1.py
@@ -14,9 +14,10 @@ except ZeroDivisionError:
     print('ERRO: Dividiu por zero')
 except NameError:
     print('ERRO: is not defined')
-except (TypeError, IndexError) as error:
+except (TypeError, IndexError) as error: # alias qual variavel será jogado o erro
     print('ERRO: TypeError + IndexError')
-    print('ERROR:', error)
+    print('ERROR:', error) # exibindo o erro definido pela as
+    print('nome:', error.__class__.__name__) # capiturando o tipo de erro (classe erro)
 except Exception:
     print('ERRO: Desconhecido')
 
