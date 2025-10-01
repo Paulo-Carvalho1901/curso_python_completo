a1a2177063c37cf422505c91400e7a5029b0aa93
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-01 10:17:41 -0300

    criado funções que cria lebanda execept para tratar erros no codigo

--- 02_funcoes_dicionarios_modulos/86_raise.py
+++ 02_funcoes_dicionarios_modulos/86_raise.py
@@ -7,11 +7,26 @@
 # raise ValueError('Deu erro!')
 # print(456)
 
-
-def divide(n, d):
+def nao_pode_ser_zero(d):
     if d == 0:
         raise ZeroDivisionError('Você esta tentando dividir por zero.')
-    
+    return True
+
+
+def deve_ser_int_float(n):
+    tipo_n = type(n)
+    if not isinstance(n, (float, int)):
+        raise TypeError(
+            f'{n} deve ser int ou flot.'
+            f'{tipo_n.__name__} enviado.'
+        )
+    return True
+
+
+def divide(n, d):
+    deve_ser_int_float(n)
+    deve_ser_int_float(d)
+    nao_pode_ser_zero(d)
     return n / d
 
-print(divide(8, 0))
+print(divide(8, '0'))
