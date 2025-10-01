79a069a764c12cef2f0809054598eef2435e98ae
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-01 00:34:51 -0300

    entendendo logica de tratativas de erros

--- 02_funcoes_dicionarios_modulos/84_try_except_part_1.py
+++ 02_funcoes_dicionarios_modulos/84_try_except_part_1.py
@@ -2,14 +2,15 @@
 
 # imprimindo The Zen of Python, by Tim Peters (python -c 'import this')
 
-a = 18
-b = 0
-# c = a / b
 
 try:
-    c = a / b # desta maneira o erro esta sendo silenciado
+    a = 18
+    b = 0
+    print('linha 1')
+    c = a / b
+    print('linha 2')
 except:
-    ...
+    print('ERRO: Dividiu por zero')
 
 
 print('continuar...')
