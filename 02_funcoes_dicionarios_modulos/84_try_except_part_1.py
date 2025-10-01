5bbc0cd87a0e963529d662806ae6891dec9bd82c
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-01 08:52:21 -0300

    utimas considerações de tratamento de erros

--- 02_funcoes_dicionarios_modulos/84_try_except_part_1.py
+++ 02_funcoes_dicionarios_modulos/84_try_except_part_1.py
@@ -2,15 +2,15 @@
 
 # imprimindo The Zen of Python, by Tim Peters (python -c 'import this')
 
-
 try:
     a = 18
     b = 0
     # print(b[0])
-    print('linha 1'[100])
+    # print('linha 1'[100])
     c = a / b
     print('linha 2')
-except ZeroDivisionError:
+except ZeroDivisionError as e:
+    print(e.__class__.__name__)
     print('ERRO: Dividiu por zero')
 except NameError:
     print('ERRO: is not defined')
