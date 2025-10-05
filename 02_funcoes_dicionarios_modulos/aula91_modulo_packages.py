c7e2508d4aa1195a5a787de12f7fd17b4c5597a1
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-05 00:21:49 -0300

    introducao ao packages

---
+++ 02_funcoes_dicionarios_modulos/aula91_modulo_packages.py
@@ -0,0 +1,14 @@
+from sys import path
+
+# Tipos de importações
+import aula91_packages
+from aula91_packages import modulo
+from aula91_packages.modulo import *
+
+
+# print(*path, sep='\n')
+
+print(modulo.soma(1, 3))
+print(aula91_packages.modulo.soma(1, 2))
+print(variavel)
+print(nova_variavel)
