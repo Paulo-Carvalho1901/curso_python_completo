781ba9fd18b3f93232570217c6351b2ac4efbc28
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-08 10:06:40 -0300

    Operador de atribuição

---
+++ pratica/Opederadores_precedencia_operadores/operador_parte3.py
@@ -0,0 +1,29 @@
+# Operadores de comparação
+
+# > maior
+# < menor
+# == igual
+# != diferente
+# >= maior ou igual
+# <= menor ou igual
+
+#EX 1
+numero_1 = 2
+numero_2 = 4
+
+soma = numero_1 + numero_2
+
+if soma < 10:
+    print("soma não é maior que 10")
+else:
+    print("soma é maior ou igual a 10")
+
+# EX2
+
+soma_1 = 7 + 8
+soma_2 = 10 + 5
+
+if soma_1 == soma_2:
+    print("Os resultados são iguais")
+else:
+    print("Os resultados são diferentes")
