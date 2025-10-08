51474d9adbbc295e26e1c98db4dfcc8f18c869cd
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-08 10:24:45 -0300

    aula de operadores

---
+++ pratica/Opederadores_precedencia_operadores/aula_operadores.py
@@ -0,0 +1,25 @@
+# links para estudos
+# https://www.devmedia.com.br/operadores-no-python/40693
+
+# DOC Python 
+# https://docs.python.org/pt-br/3.13/library/operator.html
+
+# Material VIDEO AULAS
+# https://www.youtube.com/watch?v=hvJCgDh5vrQ
+# https://www.youtube.com/watch?v=JpSDHpY4BKk&t=22s
+
+A = float(input('Digite o primeiro numero: '))
+B = float(input('Digite o segundo numero: '))
+
+soma = A + B
+substracao = A - B
+multiplicacao = A * B
+divisao = A / B
+divisao_inteira = A % B
+
+print('Resultados')
+print('Soma: ', soma)
+print('substracao: ', substracao)
+print('multiplicacao: ', multiplicacao)
+print('divisao: ', divisao)
+print('divisao_inteira', divisao_inteira)
