a6d8a74665e94de8fa7891bfa8762cac24d10046
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-08 09:58:26 -0300

    aoperadores aritimericos e seus precedentes

---
+++ pratica/Opederadores_precedencia_operadores/operador_parte1.py
@@ -0,0 +1,49 @@
+# Tipos diferentes de operadores
+
+# Operador aritmético
+# Operador de atribuição
+# Operador de comparação
+# Operador lógico
+# Operador de identidade
+# Operador de associação
+
+# + Adição ou sinal de positivo 10 + 7
+# - Subtração ou sinal negativo 10 - 7
+# * Multiplicação 3 * 4
+# / Divisão 10 / 5
+# // Divisão inteira 10 // 6
+# % Módulo 4 % 2
+# ** Exponencial 4 ** 2
+
+# Código 1
+
+numero_1 = 5
+numero_2 = 2
+
+soma = numero_1 + numero_2
+subtracao = numero_1 - numero_2
+multiplicacao = numero_1 * numero_2
+divisao = numero_1 / numero_2
+divisao_inteira = numero_1 // numero_2
+modulo = numero_1 % numero_2
+exponenciacao = numero_1 ** numero_2
+
+print(soma)
+print(subtracao)
+print(multiplicacao)
+print(divisao)
+print(divisao_inteira)
+print(modulo)
+print(exponenciacao)
+
+
+# Precedencia dos operadores em uma expressão
+
+# 1.parenteses
+print((2 + 5) ** 3)
+
+# 2.expoenciação
+print(2 + 5 ** 2)
+
+# 3.nultiplicação, divisao, divisao inteira
+print(5 * 3 + 8)
