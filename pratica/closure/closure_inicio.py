ccb7abc2ebfd3f13a8bea6d5f604dc8bc88b6068
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-16 01:04:36 -0300

    Adianda aexecução da função e deixando o parametro nome dinamico

--- pratica/closure/closure_inicio.py
+++ pratica/closure/closure_inicio.py
@@ -1,13 +1,13 @@
 # Closure e funções que retornam outras funções
 
-def criar_saudacao(saudacao, nome):
-    def saudar():
+def criar_saudacao(saudacao):
+    def saudar(nome):
         return f'{saudacao} {nome}'
     return saudar
 
 
-saudacao_1 = criar_saudacao('Bom dia', 'Paulo')
-saudacao_2 = criar_saudacao('Boa noite', 'Paulo')
+saudacao_1 = criar_saudacao('Bom dia')
+saudacao_2 = criar_saudacao('Boa noite')
 
-print(saudacao_1()) # clouse saudacao_1()
-print(saudacao_2())
+print(saudacao_1('Paulo')) # clouse saudacao_1()
+print(saudacao_2('Paulo'))
