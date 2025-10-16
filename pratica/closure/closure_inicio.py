27ab45d677579da10e5913bd9ea96e877e47dfa3
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-16 00:37:12 -0300

    criando uma função dentro de criar saudação

--- pratica/closure/closure_inicio.py
+++ pratica/closure/closure_inicio.py
@@ -1,8 +1,12 @@
 # Closure e funções que retornam outras funções
 
 def criar_saudacao(saudacao, nome):
-    return f'{saudacao} {nome}'
+    def saudar():
+        return f'{saudacao} {nome}'
+    return saudar
 
 
 saudacao_1 = criar_saudacao('Bom dia', 'Paulo')
+saudacao_2 = criar_saudacao('Boa noite', 'Paulo')
 print(saudacao_1)
+print(saudacao_2)
