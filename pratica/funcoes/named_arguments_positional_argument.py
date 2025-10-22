9983d720c8222f33b4da09b1c0bd6fbef050dbc2
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-22 17:14:47 -0300

    refactor: add new argument

--- pratica/funcoes/named_arguments_positional_argument.py
+++ pratica/funcoes/named_arguments_positional_argument.py
@@ -5,10 +5,11 @@ Unnamed arguments receive only the argument (value)
 """
 
 # function definition
-def sum(x, y):
-    print(f'{x=} {y=}', '|', 'x + y', x + y)
+def sum(x, y, z):
+    print(f'{x=} {y=} {z=}', '|', 'x + y + z', x + y + z)
 
 
 # execute the function
-sum(1, 2) # unnamed arguments (positional)
-sum(y=2, x=1) # named arguments
+sum(1, 2, 3) # unnamed arguments (positional)
+sum(y=2, z=3, x=1) # named arguments
+sum(1, 2, z=5)
