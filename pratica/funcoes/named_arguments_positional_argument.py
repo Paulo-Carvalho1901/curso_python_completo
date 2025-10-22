9c1a94cc4801d3030cd3641fc883e59ea95e906a
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-22 17:03:00 -0300

    feat: named arguments

--- pratica/funcoes/named_arguments_positional_argument.py
+++ pratica/funcoes/named_arguments_positional_argument.py
@@ -6,8 +6,9 @@ Unnamed arguments receive only the argument (value)
 
 # function definition
 def sum(x, y):
-    print(x + y)
+    print(f'{x=} {y=}', '|', 'x + y', x + y)
 
 
 # execute the function
 sum(1, 2)
+sum(2, 1)
