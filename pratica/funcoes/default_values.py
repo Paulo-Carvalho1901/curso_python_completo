56d5d0f721f6dab91820c74ea8e8e756dc47403f
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-22 17:22:55 -0300

    feat: default value Nonetype

---
+++ pratica/funcoes/default_values.py
@@ -0,0 +1,17 @@
+"""
+Default Values for Parameters
+When defining a function, parameters can have default values. If no 
+value is passed to the parameter, the default value will be used.
+Refactor: Edit your code
+"""
+
+
+
+def sum(x, y):
+    print(x + y)
+
+
+
+sum(1, 2)
+sum(3, 5)
+sum(100, 200)
