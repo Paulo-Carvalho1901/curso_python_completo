73bcc6fb9cef637cb03dad033e4a4044459bd945
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-22 14:41:46 -0300

    criando uma função nova introdução

---
+++ pratica/funcoes/introducao_definindo_funcao.py
@@ -0,0 +1,25 @@
+"""
+Introdução às funções (def) em Python
+Funções são trechos de código usados para
+replicar determinada ação ao longo do seu código.
+Elas podem receber valores para parâmetrizar (argumentos)
+e retornar um valor especifico.
+por padrão, funções Python retornam None (nada).
+"""
+# https://docs.python.org/pt-br/3.13/library/functions.html
+# https://docs.python.org/pt-br/3.13/c-api/function.html
+
+# Ex de funções
+# https://www.codaqui.dev/trilhas/python/page-5/?gad_source=1&gad_campaignid=23058687337&gclid=CjwKCAjwgeLHBhBuEiwAL5gNESFQ3Mriy2glJB9lCxtI1xftBNpCzii6xknPnm6aFBCg1bBQHGcB7RoCIxwQAvD_BwE
+
+
+# definindo uma função em python (def)
+def Print():
+    print('Variavel1')
+    print('Variavel2')
+    print('Variavel3')
+    print('Variavel4')
+
+
+Print()
+Print()
