97d5139181e3ffd581f5bc26531f2a71869ab487
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-22 16:55:36 -0300

    feat: create function sum

---
+++ pratica/funcoes/named_arguments_positional_argument.py
@@ -0,0 +1,13 @@
+"""
+Named and unnamed arguments in Python functions
+Named arguments have a name with an equal sign
+Unnamed arguments receive only the argument (value)
+"""
+
+# function definition
+def sum(x, y):
+    print(x + y)
+
+
+# execute the function
+sum(1, 2)
