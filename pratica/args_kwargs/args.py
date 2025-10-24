f0321e8ca3ff6c220d309dcdd135316e445b3a02
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-24 20:56:13 -0300

    feat: (args) realizando a soma dos argumentos

--- pratica/args_kwargs/args.py
+++ pratica/args_kwargs/args.py
@@ -4,8 +4,8 @@ args - Argumentos não nomeados
 """
 
 # Lembre-te de desempacotamento
-x, y, *resto = 1, 2, 3, 4
-print(x, y, resto)
+# x, y, *resto = 1, 2, 3, 4
+# print(x, y, resto)
 
 
 # def soma(x, y):
@@ -14,10 +14,17 @@ print(x, y, resto)
 def soma(*args):
     total = 0
     for numero in args:
-        print('Total', total, numero)
         total += numero
-        print('Total', total)
-    print(total)
+    return total
 
 
-soma(1, 2, 3, 4, 5, 6)
+soma_1_2_3 = soma(1, 2, 3)
+print(soma_1_2_3)
+
+soma_4_5_6 = soma(4, 5, 6)
+print(soma_4_5_6)
+
+outra_soma = soma(1, 2, 3, 4, 5, 6, 7, 78)
+print(outra_soma)
+
+print(sum((1, 2, 3, 4, 5, 6, 7, 78)))
