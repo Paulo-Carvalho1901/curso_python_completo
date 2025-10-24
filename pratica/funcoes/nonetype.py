007acca74969575c4eae47c6c26b85cae1494ce5
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-24 16:30:16 -0300

    fat: valores NoneType nao valores com not is not

---
+++ pratica/funcoes/nonetype.py
@@ -0,0 +1,20 @@
+7# Valores padrão para parâmetros em funções Python + NoneType e None
+
+"""
+Valores padrão para parâmetros
+Ao definir uma função, os parâmetros podem
+ter valores padrão. Caso o valor não seja
+enviado para o parâmetro, o valor padrão será
+usado.
+Refatorar: editar o seu código.
+"""
+
+def sum(x, y, z=None):
+    if z is not None:
+        print(f'{x=} {y=} {z=}', x + y + z)
+    else:
+        print(f'{x=} {y=}', x + y)
+
+sum(1, 2)
+sum(3, 5)
+sum(7, 9, 0)
