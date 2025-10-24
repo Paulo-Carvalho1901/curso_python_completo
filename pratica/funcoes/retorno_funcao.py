7abd62bd628e684afaec4a95a7b60ee9f28167e2
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-24 18:25:00 -0300

    feat: (função) return de função

---
+++ pratica/funcoes/retorno_funcao.py
@@ -0,0 +1,17 @@
+# Retorno de valores das funçoes (return)
+
+
+def soma(x, y):
+    if x > 10:
+        return [10, 20]
+    return x + y # retun
+
+
+# variavel = soma(1, 2) # None é não valor
+# variavel = int('1')
+# print(variavel)
+soma1 = soma(2, 2)
+soma2 = soma(3, 3)
+print(soma1)
+print(soma2)
+print(11, 55)
