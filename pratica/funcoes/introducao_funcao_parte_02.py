cd7b6a96f4d9efba94091b161aa6cb7a91c70936
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-24 21:13:48 -0300

    feat: (função) conceito basicos de função e sua sintax

---
+++ pratica/funcoes/introducao_funcao_parte_02.py
@@ -0,0 +1,7 @@
+# sintax básica exemplo simples
+
+def nome_da_funcao(parametro1, parametro2):
+    """Docstring: Documentaçao da função"""
+    # Corpo da função
+    # Código que será executada quando a função for chamada
+    return # valor opcional 
