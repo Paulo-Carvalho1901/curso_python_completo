54f19e30abe71404fc5a62f79ce28b1ec7af00df
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-27 18:37:17 -0300

    feat: corpo da função

---
+++ pratica/funcoes/corpo_funcao.py
@@ -0,0 +1,22 @@
+def func():
+    pass
+
+result = func()
+print(result) # O returno de uma função se nao for especificado sempre será None
+
+def add(x):
+    def inner(y):
+        return x + y
+    return inner
+
+
+print(add(1)(2))
+
+# High Order Function
+def diga_oi(nome):
+    def formata_nome():
+        return f'Olá, {nome}'
+    resultado = formata_nome()
+    return resultado
+
+print(diga_oi('Paulo'))
