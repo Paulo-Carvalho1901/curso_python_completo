b68fc1038c4eb8e71c7d32b904f60447a218066b
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-28 17:26:38 -0300

    feat: função geradora chamada avaliação preguiçosa

--- pratica/generator_python/geradores.py
+++ pratica/generator_python/geradores.py
@@ -26,11 +26,29 @@
 # print(next(g))
 
 
-def gerador():
-    val = 0
+# def gerador():
+#     val = 0
+#     while True:
+#         yield val
+#         val += 1
+
+
+# g = gerador()
+# print(next(g))
+# print(next(g))
+# print(next(g))
+
+
+"""
+Essa condição de só calcular quando for chamado,
+por conta da simetria, se chama "avaliação preguiçosa"
+[Lazy Evaluation]
+"""
+
+def gerador(start=0, step=1):
     while True:
-        yield val
-        val += 1
+        yield start
+        start += step
 
 
 g = gerador()
