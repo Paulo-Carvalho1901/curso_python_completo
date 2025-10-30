4285f73ba680f73766454d8003422e2b172828e0
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-10-30 01:56:57 -0300

    feat: forma imperativa de uma função

--- pratica/funcoes/funcoes02.py
+++ pratica/funcoes/funcoes02.py
@@ -11,6 +11,8 @@
 # * Passando como parâmetros
 # * Retornadas por funções
 
+# Função que recebe outra função
+# High Order Function
 def apply(func, val):
     return func(val)
 
@@ -21,3 +23,21 @@ def soma_mais_um(x):
 
 print(apply(soma_mais_um, 1))
 
+# Esse é um conceitos por trás de diversas funções
+# da biblioteca padrão do Python como:
+
+# map, filter, sorted, functools.reduce, itertoos.takewhile
+
+# Funções vs loops
+# Uma das formas mais comuns de usar funções como objeto é
+# poder evitar o uso de loops como for e while. Transformando a maneira
+# imperativa de se programar, para uma forma declarativa
+
+# Imperativa
+seq = [1, 2, 3, 4, 5]
+result = []
+
+for val in seq:
+    result.append(val ** 2)
+
+print(result)
