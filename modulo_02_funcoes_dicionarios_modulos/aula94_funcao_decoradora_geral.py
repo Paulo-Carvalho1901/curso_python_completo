00da220b5b38aaad2260b67c06c80a5ad2feb9f0
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-11-04 22:07:47 -0300

    refector: explicando conceitos de função decoradoras

--- modulo_02_funcoes_dicionarios_modulos/aula94_funcao_decoradora_geral.py
+++ modulo_02_funcoes_dicionarios_modulos/aula94_funcao_decoradora_geral.py
@@ -4,11 +4,27 @@
 # Decoradores são usados para fazer o Python
 # usar as funções decoradoras em outras funções
 
+# Função decoradora
+def criar_funcao(func):
+    def interna(*args, **kwargs):
+        for arg in args:
+            e_string(arg)
+        resultado = func(*args, **kwargs)
+        return resultado
+    return interna # clousure
 
 
 def inverte_string(string):
     return string[::-1]
 
 
-invertida = inverte_string('Paulo')
+def e_string(param):
+    if not isinstance(param, str):
+        raise TypeError('Parametro deve ser uma string!')
+
+
+
+
+invertendo_string_checando_parametro = criar_funcao(inverte_string)
+invertida = invertendo_string_checando_parametro(123)
 print(invertida)
