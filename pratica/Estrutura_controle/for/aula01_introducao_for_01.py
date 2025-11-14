d3c409007e96a80dc088054a83616326f907d2ba
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-11-14 02:32:02 -0300

    feat: while com senha entendendo conceito de um loop

--- pratica/Estrutura_controle/for/aula01_introducao_for_01.py
+++ pratica/Estrutura_controle/for/aula01_introducao_for_01.py
@@ -14,3 +14,17 @@ while i < tamanho_string:
     print(texto[i])
 
     i += 1
+
+print()
+# exemplo de while com senha
+
+senha_salva = '1234'
+senha_digitada = ''
+repeticoes = 0
+
+while senha_salva != senha_digitada:
+    senha_digitada = input(f'Sua senha ({repeticoes}x): ')
+
+    repeticoes += 1
+
+print('Aquele laço acima pode ter repetições infinitas!')
