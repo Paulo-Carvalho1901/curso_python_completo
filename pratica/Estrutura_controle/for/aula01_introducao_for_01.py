3feb042e8a324d5fc334d63f56037048c335771a
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-11-14 02:37:26 -0300

    feat: primeiro exemplo de for laço de repetição

--- pratica/Estrutura_controle/for/aula01_introducao_for_01.py
+++ pratica/Estrutura_controle/for/aula01_introducao_for_01.py
@@ -5,27 +5,37 @@ Geralmente a estrutura while se utiliza com
 iterações que não sabemos sua parada (quando éla termina)
 """
 
-texto = 'Python'
+# texto = 'Python'
 
-i = 0
-tamanho_string = len(texto)
+# i = 0
+# tamanho_string = len(texto)
 
-while i < tamanho_string:
-    print(texto[i])
+# while i < tamanho_string:
+#     print(texto[i])
 
-    i += 1
+#     i += 1
 
-print()
+# print()
 # exemplo de while com senha
 
-senha_salva = '1234'
-senha_digitada = ''
-repeticoes = 0
+# senha_salva = '1234'
+# senha_digitada = ''
+# repeticoes = 0
 
-while senha_salva != senha_digitada:
-    senha_digitada = input(f'Sua senha ({repeticoes}x): ')
+# while senha_salva != senha_digitada:
+#     senha_digitada = input(f'Sua senha ({repeticoes}x): ')
 
-    repeticoes += 1
+#     repeticoes += 1
 
-print('Teve', repeticoes, 'repetiçoes')
-print('Aquele laço acima pode ter repetições infinitas!')
+# print('Teve', repeticoes, 'repetiçoes')
+# print('Aquele laço acima pode ter repetições infinitas!')
+
+
+"""
+Agora com o for é para as vezes que sabemos quantas iterações (repetiçoes terá)
+"""
+
+string = 'Python'
+
+for letra in string:
+    print(letra)
