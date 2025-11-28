a06c1001d8e9a2a20c61e6c1296932d3516046dc
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-11-27 22:04:57 -0300

    criando uma deepcopy de uma lista de produto

--- pratica/deepcopy/aumenta_preco.py
+++ pratica/deepcopy/aumenta_preco.py
@@ -6,11 +6,21 @@ produtos = [
     {'nome': 'Produto 4', 'preco': 69.90},
 ]
 
+import copy # importe para deepcopy
+
 # copy, sorted, produtos.sort
 # Exercícios
 # Aumente os preços dos produtos a seguir em 10%
 # Gere novos_produtos por deep copy (cópia profunda)
 
+novos_produtos = [
+    {**produto, 'preco': round(produto['preco'] * 1.1, 2)}
+    for produto in copy.deepcopy(produtos)
+]
+
+print(*produtos, sep='\n')
+print()
+print(*novos_produtos, sep='\n')
 
 # Ordene os produtos por nome decrescente (do maior para menor)
 # Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
@@ -18,5 +28,3 @@ produtos = [
 
 # Ordene os produtos por preco crescente (do menor para maior)
 # Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
-
-
