17f92f6b54661e3ef90f3d976a75a87fbac69116
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-11-30 22:53:36 -0300

    varificando a variavel livre

--- pratica/freevars/freevars.py
+++ pratica/freevars/freevars.py
@@ -1,9 +1,11 @@
-# Variaveis livres + nonlocal
+# Variaveis livres + nonlocal (locals, globals)
 
 def fora(x):
     a = x # a é uma varivel livre
 
     def dentro():
+        # print(locals())
+        print(dentro.__code__.co_freevars)
         return a
     return dentro
 
