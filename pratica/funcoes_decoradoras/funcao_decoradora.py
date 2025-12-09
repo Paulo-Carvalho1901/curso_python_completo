f2e249d9619c4603562b9ca12fb9a1ab82cb5575
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-12-08 23:37:19 -0300

    feat: entendendo sobre decorador

--- pratica/funcoes_decoradoras/funcao_decoradora.py
+++ pratica/funcoes_decoradoras/funcao_decoradora.py
@@ -7,22 +7,26 @@
 
 def criar_funcao(func):
     def interna(*args, **kwargs):
-        print('Vou te decorar!')
+        print('Vou te decorar')
         for arg in args:
             e_string(arg)
         resultado = func(*args, **kwargs)
-        print('Ok, você foi decorador!')
+        print(f'O seu resultado foi {resultado}')
+        print('OK, agora você foi decorado.')
         return resultado
     return interna
 
 # função que inverte string
+@criar_funcao
 def inverte_string(string):
+    print(f'{inverte_string.__name__}')
     return string[::-1]
+
 
 def e_string(param):
     if not isinstance(param, str):
         raise TypeError('Parametro deve ser uma string!')
 
-inverte_string_checando_parametro = criar_funcao(inverte_string)
-invertida = inverte_string_checando_parametro('123')
+
+invertida = inverte_string('123')
 print(invertida)
