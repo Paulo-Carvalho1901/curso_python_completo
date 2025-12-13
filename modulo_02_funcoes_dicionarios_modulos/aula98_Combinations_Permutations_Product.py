9bd3213540fe9a642d314cecf33f652aa1099304
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-12-13 02:32:42 -0300

    feat: criando combinations

---
+++ modulo_02_funcoes_dicionarios_modulos/aula98_Combinations_Permutations_Product.py
@@ -0,0 +1,20 @@
+# Combinations, Permutations e Product - Itertools
+# Combinação - Ordem não importa - iterável + tamanho do grupo
+# Permutação - Ordem importa
+# Produto - Ordem importa e repete valores únicos
+
+from itertools import combinations
+
+def print_iter(iterator):
+    print(*list(iterator), sep='\n')
+    print()
+
+pessoas = [
+    'joao', 'joana', 'luiz', 'leticia'
+]
+
+camiseta = [
+    ['preta', 'branca']
+]
+
+print_iter(combinations(pessoas, 2))
