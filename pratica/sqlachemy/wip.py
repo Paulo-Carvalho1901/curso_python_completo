11fd758e8a3d8c63edcaf4b037b2b551d13c3872
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-12-14 17:35:32 -0300

    feat: entendendo engine

---
+++ pratica/sqlachemy/wip.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine # fabrica de conexões
+ 
+# Criando banco de dados em memória
+engine = create_engine(
+    #'postgresql+psycopg://app_user:app_password@localhost:5432/app_db'
+    'sqlite://'
+)
+
+print(engine)
+print(engine.dialect)
