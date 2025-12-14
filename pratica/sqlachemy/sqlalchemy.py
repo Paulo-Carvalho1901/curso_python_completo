2a75d1f7c908c6ba3e8d50d1e22f16b4158b713c
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-12-14 00:43:24 -0300

    Docs: documentação sobre sqlalchemy

---
+++ pratica/sqlachemy/sqlalchemy.py
@@ -0,0 +1,17 @@
+"""
+SQLAlchemy
+
+Um kit ferramentas para trabahar com banco de dados SQL e Python
+A ideia principal é abstrair as chamadas para drivers especificos
+de banco de dados.
+
+O toolkit suporta:
+
+* Async e Sync
+* Anotações de tipos e forma dinâmica
+* implementa a DBAPI (PEP 249)
+* Suporte cPython PyPy
+* ORM
+* Sistemas de plugins
+* Eventos 
+"""
