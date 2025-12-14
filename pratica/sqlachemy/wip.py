918a23fc0f7f8eb7e8240c1cc8b0c662c966612d
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-12-14 18:51:06 -0300

    feat: entendendo a conexao

--- pratica/sqlachemy/wip.py
+++ pratica/sqlachemy/wip.py
@@ -3,20 +3,10 @@ from sqlalchemy import create_engine # fabrica de conexões
 # Criando banco de dados em memória
 engine = create_engine(
     #'postgresql+psycopg://app_user:app_password@localhost:5432/app_db'
+    # echo=True
     'sqlite://'
 )
 
-print(engine.pool)
+conn = engine.connect()
 
-# print(engine)
-# print(engine.dialect)
-
-conn1 = engine.connect()
-conn2 = engine.connect()
-conn1.close
-conn2.close
-conn3 = engine.connect()
-
-print(engine.pool.status())
-
-# conn.close()
+conn.close()
