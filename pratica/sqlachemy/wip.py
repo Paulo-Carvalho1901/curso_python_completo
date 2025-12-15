16109442a74bac563092814183dccfdd648bf812
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-12-15 00:14:43 -0300

    feat: criado tabela no ORM

--- pratica/sqlachemy/wip.py
+++ pratica/sqlachemy/wip.py
@@ -9,4 +9,12 @@ class Base(declarative_base):
 class Comment(Base):
     __tablename__ = 'comments'
 
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    Comment = Column(String, nullable=False)
+    live = Column(String, nullable=False)
+    created_at = Column(DateTime, server_default=func.now())
+
+    def __repr__(self) -> str:
+        return f'comment({self.id=}, {self.name=}, {self.comment=}, {self.live=}, {self.created_at=})'
     
