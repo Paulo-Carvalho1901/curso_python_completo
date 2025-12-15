3e5a03cd2138ebff0bf1b72a1cec6dbc85437f15
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-12-15 00:53:17 -0300

    feat: criado conexao com session

--- pratica/sqlachemy/wip.py
+++ pratica/sqlachemy/wip.py
@@ -1,10 +1,13 @@
 from datetime import datetime
 
 from sqlalchemy import func
-from sqlalchemy.orm import Mapped, mapped_column, registry
+from sqlalchemy.orm import Mapped, mapped_column, registry, Session
+from sqlalchemy import create_engine, select
+
+
+engine = create_engine('sqlite:///database.db')
 
 reg = registry()
-
 
 @reg.mapped_as_dataclass
 class Comment:
@@ -17,3 +20,16 @@ class Comment:
     created_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now()
     )
+
+
+Comment(
+    name='Paulo',
+    comment='bla',
+    live='youtube'
+)
+
+
+with Session(engine) as s:
+    result = s.scalar(select(Comment).where(Comment.id == 1))
+    s.delete(result)
+    s.commit()
