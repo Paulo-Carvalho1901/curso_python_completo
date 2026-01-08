d399fd41ef5d8223c0c32b63f77a50a0b5798554
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-01-08 01:30:48 -0300

    feat: criando uma função acumuladora total

---
+++ modulo_02_funcoes_dicionarios_modulos/aula102_reduce.py
@@ -0,0 +1,18 @@
+# reduce - faz a redução de um iteravel em um valor
+# reduzindo um iteravel em um unico valor
+
+from functools import reduce
+
+produtos = [
+    {'nome': 'Produto 5', 'preco': 10.00},
+    {'nome': 'Produto 1', 'preco': 22.32},
+    {'nome': 'Produto 3', 'preco': 10.11},
+    {'nome': 'Produto 2', 'preco': 105.87},
+    {'nome': 'Produto 4', 'preco': 69.90},
+]
+
+total = 0
+for p in produtos:
+    total += p['preco']
+
+print(total)
