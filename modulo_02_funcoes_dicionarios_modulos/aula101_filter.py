104887f82b43f51e61bfe1de28d66cba9f02fc83
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-01-08 01:24:29 -0300

    feat: utilizando os filter

---
+++ modulo_02_funcoes_dicionarios_modulos/aula101_filter.py
@@ -0,0 +1,35 @@
+# filter é um filtro funcional
+
+def print_inter(iterator):
+    print(*list(iterator), sep='\n')
+    print()
+
+
+produtos = [
+    {'nome': 'Produto 5', 'preco': 10.00},
+    {'nome': 'Produto 1', 'preco': 22.32},
+    {'nome': 'Produto 3', 'preco': 10.11},
+    {'nome': 'Produto 2', 'preco': 105.87},
+    {'nome': 'Produto 4', 'preco': 69.90},
+]
+
+
+def filtra_preco(produto):
+    return produto['preco'] > 100
+
+# filter
+# novos_produtos = [
+#     p for p in produtos
+#     if p['preco'] > 100
+# ]
+
+
+novos_produtos = filter(
+    # lambda p: p['preco'] > 100,
+    filtra_preco,
+    produtos
+)
+
+
+print_inter(produtos)
+print_inter(novos_produtos)
